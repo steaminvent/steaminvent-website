d29228d6bfbdbf8a2c7578b4bfe1e7b244e965d5
steaminvent <steaminvent.sm@gmail.com>
2026-09-19 18:13:33 +0200

    Add XML sitemap generation

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://steaminvent.org',
+
+  integrations: [
+    sitemap()
+  ],
+
   vite: {
     plugins: [tailwindcss()]
   }
